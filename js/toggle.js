eb9ab1b70610dccf4ad83eaeb962427b9b3552e5
Sahil Bisht <Sahilbisht19334@gmail.com>
2022-09-10 17:51:01 +0530

    fix some minor issue

--- js/toggle.js
+++ js/toggle.js
@@ -1,5 +1,7 @@
 const toggle=document.getElementsByClassName("navbar__toggle")[0];
 const navLinks=document.getElementsByClassName('navbar__menu')[0]
+const mainContent=document.getElementsByClassName('page-title')[0]
 toggle.addEventListener('click',()=>{
     navLinks.classList.toggle('active');
+    mainContent.classList.toggle('do');
 })
